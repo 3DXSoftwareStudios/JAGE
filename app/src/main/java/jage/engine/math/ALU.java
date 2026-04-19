b75058a85e63ad919a59c71177b53ffb3bd42e9d
Anton Jantos <antonjantos1604@gmail.com>
2026-04-19 12:41:05 +0200

    Added Basis for ALU Implementation

---
+++ app/src/main/java/jage/engine/math/ALU.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2026 Anton Jantos
+ *
+ * Licensed under the Apache License, Version 2.0.
+ * See the LICENSE file in the project root for details.
+ */
+package jage.engine.math;
+
+public class ALU {
+    
+    public static int signedAdd32(int i1, int i2){
+        return i1 + i2;
+    }
+    
+}
